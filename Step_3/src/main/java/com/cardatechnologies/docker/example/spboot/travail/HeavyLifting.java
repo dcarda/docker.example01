8be3d23961460c926302b56016725b470e440825
Daniel Carda <GoneBy2025@CardaTechnologies.com>
2020-06-13 13:44:28 -0600

    Initial add of the Step 3 directory.

---
+++ Step_3/src/main/java/com/cardatechnologies/docker/example/spboot/travail/HeavyLifting.java
@@ -0,0 +1,9 @@
+package com.cardatechnologies.docker.example.spboot.travail;
+
+public class HeavyLifting {
+
+    public String runIt(){
+
+        return( "Hello Everybody!!!");
+    }
+}
---
+++ Step_3/src/test/java/com/cardatechnologies/docker/example/spboot/SpbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardatechnologies.docker.example.spboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
