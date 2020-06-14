2e8746a5e1b7bffe3ee609e003088a6b42e293fb
Daniel Carda <GoneBy2025@CardaTechnologies.com>
2020-06-14 13:54:16 -0600

    Create run.cmd
    Added this file to make starting the container easier.

---
+++ Step_1/run.cmd
@@ -0,0 +1,7 @@
+cls
+
+::  Provided the build as successful, this will actually start up the container.
+::  You can run out to the following address and look at the JBoss Admin console.
+::      http://localhost:9990/console/index.html
+::
+docker run -p 8080:8080 -p 9990:9990  -it   codewarrior23/personal-repository:wildfly-step1
