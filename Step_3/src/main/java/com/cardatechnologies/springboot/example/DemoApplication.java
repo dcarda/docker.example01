861a0b081fa499c2c70d9f3300bc5b64f8a3d420
Daniel Carda <GoneBy2025@CardaTechnologies.com>
2020-06-15 11:53:13 -0600

    Create DemoApplication.java
    Updated Formatting and comments.

---
+++ Step_3/src/main/java/com/cardatechnologies/springboot/example/DemoApplication.java
@@ -0,0 +1,23 @@
+/* ***************************************************************************************
+ * Class:  com.cardatechnologies.springboot.example.DemoApplication.java
+ * Date:   2020/06/15
+ * ***************************************************************************************
+ */
+package com.cardatechnologies.springboot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
+
+/* ***************************************************************************************
+ * Class:  com.cardatechnologies.springboot.example.DemoApplication.java
+ * Date:   2020/06/15
+ * ************************************************************************************* */
