cd9a66653f39b5dbc0f8d96fe56f3df9ede01278
Daniel Carda <GoneBy2025@CardaTechnologies.com>
2020-06-15 11:58:04 -0600

    Create these files.
    Initial Load

---
+++ Step_3/src/main/java/com/cardatechnologies/springboot/example/travail/HeavyLifting.java
@@ -0,0 +1,17 @@
+/* ***************************************************************************************
+ * Class:  com.cardatechnologies.springboot.example.travail.HeavyLifting.java
+ * Date:   2020/06/15
+ * ***************************************************************************************
+ */
+package com.cardatechnologies.springboot.example.travail;
+
+public class HeavyLifting {
+    public String runIt() {
+        return ("You Did It!!!");
+    }
+}
+
+/* ***************************************************************************************
+ * Class:  com.cardatechnologies.springboot.example.travail.HeavyLifting.java
+ * Date:   2020/06/15
+ * ************************************************************************************* */
---
+++ Step_3/src/test/java/com/cardatechnologies/springboot/example/DemoApplicationTests.java
@@ -0,0 +1,23 @@
+/* ***************************************************************************************
+ * Class:  com.cardatechnologies.springboot.example.DemoApplicationTests.java
+ * Date:   2020/06/15
+ * ***************************************************************************************
+ */
+package com.cardatechnologies.springboot.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
+/* ***************************************************************************************
+ * Class:  com.cardatechnologies.springboot.example.DemoApplicationTests.java
+ * Date:   2020/06/15
+ * ************************************************************************************* */
